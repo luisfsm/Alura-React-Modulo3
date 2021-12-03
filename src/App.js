2b248614b6c7f30fc195461214f32ca8c5929dd4
luis felipe <luis.s.mendes20@gmail.com>
2021-12-03 14:12:52 -0300

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import "fontsource-roboto";
 import Formulario from './Components/formulario';
+import { Container } from '@material-ui/core/'
+import Typography from '@material-ui/core/Typography'
+
 function App() {
   return (
     <>
-      <h1>Teste Formulario</h1>
-      <Formulario />
+      <Container component={"article"} maxWidth="xs">
+
+        <Typography variant="h3" align='center' component="h1" color="initial">Formulario Cadastro</Typography>
+        <Formulario />
+
+      </Container>
+
     </>
   );
 }
--- src/Components/formulario/Formulario.jsx
+++ src/Components/formulario/Formulario.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './Formulario.css'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Switch from '@material-ui/core/Switch'
+import Box from '@material-ui/core/Box'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
 
 function Formulario() {
     return (
         <>
-            <form action="" method="post">
-                <input type="text" placeholder='cadatroPessoa'/>
+
+            <form method="post">
+                <TextField id="Nome" label="Nome" variant="outlined" fullWidth margin='normal' />
+
+
+                <TextField id="SobreNome" label="Sobre Nome" variant="outlined" fullWidth margin='normal' />
+
+                <TextField id="cpf" label="CPF" variant="outlined" fullWidth margin='normal' />
+
+                <FormControlLabel label="Promoções" control={<Switch name="Promocoes" defaultChecked color='primary' />} />
+
+                <FormControlLabel label="Novidades" control={<Switch name="Novidades" defaultChecked color='primary' />} />
+
+                <Box display="flex" justifyContent="center" mt={2}>
+                    <Button variant='contained' color='primary' > Cadastrar</Button>
+                </Box>
 
             </form>
         </>
