c67877b94aede84e5b39ecc4896e2c4a3dbb4d53
luis felipe <luis.s.mendes20@gmail.com>
2021-12-03 14:55:39 -0300

    add hooks state

--- src/Components/formulario/Formulario.jsx
+++ src/Components/formulario/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Formulario.css'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
@@ -8,15 +8,25 @@ import FormControlLabel from '@material-ui/core/FormControlLabel'
 
 
 function Formulario() {
+    const [Nome, setNome] = useState();
+    const [SobreNome, setSobreNome] = useState(" ")
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(Nome);
+        console.log(SobreNome)
+    }
+
     return (
         <>
 
-            <form method="post">
-                <TextField id="Nome" label="Nome" variant="outlined" fullWidth margin='normal' />
+            <form method="post" onSubmit={onSubmit}>
+                <TextField id="Nome" label="Nome" variant="outlined" fullWidth margin='normal'
+                    onChange={(event) => { setNome(event.target.value) }} />
 
-
-                <TextField id="SobreNome" label="Sobre Nome" variant="outlined" fullWidth margin='normal' />
-
+                <TextField id="SobreNome" label="Sobre Nome" variant="outlined" fullWidth margin='normal'
+                onChange={(event) => { setSobreNome(event.target.value) }} />
                 <TextField id="cpf" label="CPF" variant="outlined" fullWidth margin='normal' />
 
                 <FormControlLabel label="Promoções" control={<Switch name="Promocoes" defaultChecked color='primary' />} />
@@ -24,7 +34,7 @@ function Formulario() {
                 <FormControlLabel label="Novidades" control={<Switch name="Novidades" defaultChecked color='primary' />} />
 
                 <Box display="flex" justifyContent="center" mt={2}>
-                    <Button variant='contained' color='primary' > Cadastrar</Button>
+                    <Button variant='contained' type="submit" color='primary' > Cadastrar</Button>
                 </Box>
 
             </form>
