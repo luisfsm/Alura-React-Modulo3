5a920dad63061eb5dc24d788d988071d4227b780
luis felipe <luis.s.mendes20@gmail.com>
2021-12-03 13:08:56 -0300

    Projeto inicial

---
+++ src/Components/formulario/Formulario.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Formulario.css'
+
+function Formulario() {
+    return (
+        <>
+            <form action="" method="post">
+                <input type="text" placeholder='cadatroPessoa'/>
+
+            </form>
+        </>
+    )
+}
+
+export default Formulario
